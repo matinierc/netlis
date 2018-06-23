ff764b5430d589cd0488ac65d9ab90eba645beea
Cyrille MATINIER <cyrille.matinier@gmail.com>
2018-06-23 17:18:06 +0200

    no message

--- src/netlis/Netlis.java
+++ src/netlis/Netlis.java
@@ -1,9 +1,12 @@
 package netlis;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Netlis {
+  private static Logger logger = LogManager.getLogger(Netlis.class);
 
   public static void main(String[] args) {
-    System.out.println("This is the first time");
+    logger.info("My first log");
   }
 }
---
+++ src/netlis/ui/NetlisUI.java
@@ -0,0 +1,11 @@
+package netlis.ui;
+
+import javax.swing.JFrame;
+
+public class NetlisUI {
+  protected final JFrame frame = new JFrame();
+  
+  public void render() {
+    
+  }
+}
